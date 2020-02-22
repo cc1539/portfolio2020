9921e35c34c2e32144f641df79beb4c5e40e413d
cc1539 <43189993+cc1539@users.noreply.github.com>
2020-02-22 14:30:32 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+
+window.onload = function() {
+	
+	
+	// animate the title card
+	let title = document.getElementById("title-section").children[0];
+	let title_text = title.innerHTML;
+	for(let i=title_text.length-1;i>0;i--) {
+		title_text =
+			title_text.substring(0,i)+
+			"</span><span>"+
+			title_text.substring(i);
+	}
+	title.innerHTML = "<span>"+title_text+"</span>";
+	for(let i=0;i<title.children.length;i++) {
+		title.children[i].style.position = "relative";
+	}
+	setInterval(function(){
+		for(let i=0;i<title.children.length;i++) {
+			title.children[i].style.top = (Math.sin(i*.4+Date.now()/3e2)*10)+"px";
+		}
+	},10);
+	
+	// add in the project cards
+	let projects = document.getElementById("project-cards");
+	var xhttp = new XMLHttpRequest();
+	xhttp.overrideMimeType("application/json");
+	xhttp.open("GET","data/projects.json");
+	xhttp.onreadystatechange = function() {
+		if(xhttp.readyState==4 && xhttp.status==200) {
+			
+		}
+	};
+	xhttp.onerror = function() {
+		console.log("error");
+	};
+	xhttp.send();
+	
+};
