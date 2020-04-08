22b443f16baf0f77c89802b3d2489f5329149c49
unknown <metash.ian@gmail.com>
2020-04-08 00:27:22 -0400

    fix mobile visual for projects

--- main.js
+++ main.js
@@ -5,9 +5,15 @@ function manageMenuPanel() {
 	if(window.innerWidth>840) {
 		menu_panel.style = "left:0;";
 		content_panel.style = ""
+		document.querySelector(".project-image-panel,.project-desc").forEach(function(elem){
+			elem.style = "";
+		});
 	} else {
 		menu_panel.style = "";
-		content_panel.style = "width:calc(100% - 35px);"
+		content_panel.style = "width:calc(100% - 35px);";
+		document.querySelector(".project-image-panel,.project-desc").forEach(function(elem){
+			elem.style = "float:top; width:100%; height:50%;";
+		});
 	}
 }
 
